fa5a8506a5a500cfdcc9c7d24aaa51a4f8682c80
aanger258 <cosmin.g.musteata@gmail.com>
2017-09-19 18:08:39 +0300

    View

--- app/Controllers/AdminController.php
+++ app/Controllers/AdminController.php
@@ -3,11 +3,15 @@
 	namespace App\Controllers;
 
 	use Framework\HTTP\Request\Classes\Request;
+	use Framework\HTTP\View\Classes\BaseController;
 
-	class AdminController{
+	class AdminController extends BaseController{
 
 		public function index(){
 			$id = Request::getParamsExcept(array('id'));
+			BaseController::view("menu.html", $id);
 		}
+
+
 
 	}
--- config/Settings.php
+++ config/Settings.php
@@ -1,3 +1,4 @@
 <?php
 	
 	define('ROOT_FOLDER', __DIR__.'/../');
+	define("VIEW_PATH", __DIR__ . "/../app/view/");
